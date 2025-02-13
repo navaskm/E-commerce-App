6a97a730ad7301a399c9bdd09a5fc140a9297d91
Navas KM <navaskm228@gmail.com>
2025-02-13 02:00:58 -0800

    Update Links.tsx

--- src/app/components/HomePage/navbar/Links/Links.tsx
+++ src/app/components/HomePage/navbar/Links/Links.tsx
@@ -7,8 +7,8 @@ function Links({open}:Menu) {
   return(
     <ul className={`nav-links ${open? 'active':''}`}>
       <li><Link href="/">Home</Link></li>
-      <li><Link href="/components/checkoutpage">Cart</Link></li>
-      <li><Link href="/components/orderpage">Your order</Link></li>
+      <li><Link href="/components/CheckoutPage">Cart</Link></li>
+      <li><Link href="/components/OrderPage">Your order</Link></li>
     </ul>
   )
 }
