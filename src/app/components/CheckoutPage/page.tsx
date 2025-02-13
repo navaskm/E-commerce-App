966c8c474366f7383a9f8734052ffe0e5322838e
Navas KM <navaskm228@gmail.com>
2025-02-13 00:54:00 -0800

    fix checkout page

--- src/app/components/CheckoutPage/page.tsx
+++ src/app/components/CheckoutPage/page.tsx
@@ -6,8 +6,8 @@ import { Rings } from 'react-loading-icons';
 
 const NavbarSkeleton = lazy(()=> import("../HomePage/navbar/Skeleton/NavbarSkeleton"));
 const CartItems = lazy(()=> import('./CartItems/page'));
-const PaymentPage = lazy(()=> import('./paymentsection/page'));
-const TopBarOfCartPage = lazy(()=> import('./topbar/topbar'));
+const PaymentPage = lazy(()=> import('./PaymentSection/page'));
+const TopBarOfCartPage = lazy(()=> import('./TopBar/TopBar'));
 import "@/app/styles/checkoutpage/checkout.scss";
 
 const CartPage = () => {
