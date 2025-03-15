523929414e5d6e21c9fce239d78c4cff93900f00
navaskmsanu <navasnavaskm064@gmail.com>
2025-03-15 12:24:07 -0700

    create auhtentication

--- src/app/components/HomePage/navbar/Login/Login.tsx
+++ src/app/components/HomePage/navbar/Login/Login.tsx
@@ -1,3 +1,21 @@
-const Login = () => <button className="login-button">Login</button>
+import {
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/nextjs';
+
+const Login = () => {
+  return(
+    <>
+      <SignedOut>
+        <SignInButton mode="modal" />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </>
+  )
+}
 
 export default Login
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import StoreProvider from "./storeprovider";
 import "./globals.scss";
+import {ClerkProvider} from '@clerk/nextjs';
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -25,24 +26,26 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <ClerkProvider>
+      <html lang="en">
 
-      <head>
-        <meta name="description"/>
-        <link 
-          href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css"
-          rel="stylesheet" 
-          integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossOrigin="anonymous"
-        />
-          <link rel="icon" href="/Logo/title.svg" type="image/svg+xml" /> 
-      </head>
+        <head>
+          <meta name="description"/>
+          <link 
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css"
+            rel="stylesheet" 
+            integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossOrigin="anonymous"
+          />
+            <link rel="icon" href="/Logo/title.svg" type="image/svg+xml" /> 
+        </head>
 
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        <StoreProvider>
-          {children}
-        </StoreProvider>
-      </body>
+        <body className={`${geistSans.variable} ${geistMono.variable}`}>
+          <StoreProvider>
+            {children}
+          </StoreProvider>
+        </body>
 
-    </html>
+      </html>
+    </ClerkProvider>
   );
 }
