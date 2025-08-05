66db353db6580510e07111225ab4dc48ba68b54e
navaskmsanu <navasnavaskm064@gmail.com>
2025-08-05 11:20:01 -0700

    udatad again

--- src/app/components/HomePage/HomePageScrolling/BagSports/BagSports.tsx
+++ src/app/components/HomePage/HomePageScrolling/BagSports/BagSports.tsx
@@ -40,56 +40,52 @@ function BagsSports({product,order}:BagsSports) {
 
       <button className="scroll-button left" onClick={()=>scrolling('left')}>←</button>
 
-        <div className={`scrolling-products-${order}`}>
-          {
-            product.map((item:Products) =>{
-              return(
-                <div  key={item.id}>
+      <div className={`scrolling-products-${order}`}>
+        {product.map((item:Products) => (
+          <div  key={item.id}>
 
-                  <Link 
-                    style={
-                      {
-                        textDecoration:"none",
-                        display: 'flex',
-                        flexDirection: 'column',
-                        alignItems: 'center',
-                        width: '100%',
-                        height: '100%',
-                      }
-                    } 
+            <Link 
+              style={
+                {
+                  textDecoration:"none",
+                  display: 'flex',
+                  flexDirection: 'column',
+                  alignItems: 'center',
+                  width: '100%',
+                  height: '100%',
+                }
+              } 
 
-                    href={{
-                      pathname: "/components/SelectedPage",
-                      query: {
-                        name: encodeURIComponent(item.name),
-                        priceCents: item.priceCents,
-                        rating: item.rating,
-                        id: item.id,
-                        type: item.type,
-                        keywords: item.keywords,
-                        image: encodeURIComponent(item.image),
-                        company: encodeURIComponent(item.company),
-                        madein: encodeURIComponent(item.madein),
-                        Feature: encodeURIComponent(item.Feature),
-                        size: item.size,
-                      },
-                    }}>
-                  
-                    <div className="image-wrapper">
-                      <img src={item.image} alt={item.name} />
-                      <h5>{item.name}</h5>
-                      <div className="scrolling-image-offer">
-                        <h4>{item.offer}</h4>
-                      </div>
-                    </div>
-
-                  </Link>
-                  
+              href={{
+                pathname: "/components/SelectedPage",
+                query: {
+                  name: encodeURIComponent(item.name),
+                  priceCents: item.priceCents,
+                  rating: item.rating,
+                  id: item.id,
+                  type: item.type,
+                  keywords: item.keywords,
+                  image: encodeURIComponent(item.image),
+                  company: encodeURIComponent(item.company),
+                  madein: encodeURIComponent(item.madein),
+                  Feature: encodeURIComponent(item.Feature),
+                  size: item.size,
+                },
+              }}>
+            
+              <div className="image-wrapper">
+                <img src={item.image} alt={item.name} />
+                <h5>{item.name}</h5>
+                <div className="scrolling-image-offer">
+                  <h4>{item.offer}</h4>
                 </div>
-              )
-            })
-          }
-        </div>
+              </div>
+
+            </Link>
+            
+          </div>
+        ))}
+      </div>
 
       <button className="scroll-button right" onClick={()=>scrolling('right')}>→</button>
 
