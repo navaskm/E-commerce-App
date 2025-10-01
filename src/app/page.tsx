12c2b24f2cc0d3ea36e5ab57d7198b9de3d638b2
navaskm <navaskm228@gmail.com>
2025-09-30 21:06:17 -0700

    set scrolling skeleton in to the skeleton file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ const responseOfScrollingProducts = await fetchScrollingProduct();
 //Loading imports
 import NavbarSkeleton from "../skeletons/NavbarSkeleton";
 import SmallProductsSkeleton from "../skeletons/SmallProductsSkeleton";
-import ScrollingSkeleton from "./components/HomePage/HomePageScrolling/Skeleton/ScrollingSkeleton";
+import ScrollingSkeleton from "../skeletons/ScrollingSkeleton";
 import LargeProductSkeleton from "./components/HomePage/LargeProducts/Skeleton/LargeProductSkeleton";
 
 // component imports
