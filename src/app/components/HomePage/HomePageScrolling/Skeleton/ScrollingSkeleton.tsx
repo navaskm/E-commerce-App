ec0abde88a4e1efbd8184ee3bd9dd6a09a10ea27
navaskm <navaskm228@gmail.com>
2025-09-30 20:57:15 -0700

    change scrolling skeleton

--- src/app/components/HomePage/HomePageScrolling/Skeleton/ScrollingSkeleton.tsx
+++ src/app/components/HomePage/HomePageScrolling/Skeleton/ScrollingSkeleton.tsx
@@ -1,22 +1,25 @@
-import Skeleton from 'react-loading-skeleton';
-import 'react-loading-skeleton/dist/skeleton.css';
-
-import '@/app/components/HomePage/HomePageScrolling/Skeleton/Skeleton.scss'
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 const ScrollingSkeleton = () => {
   return (
-    <div className='homepage-scrolling'>
-      <div className="scrolling-product-container">
-
-      <div className="scrolling-products">
+    <div className="container-fluid bg-white mb-3 px-2">
+      <div className="d-flex overflow-auto">
         {[...Array(7)].map((_, index) => (
-          <div key={index} className="skeleton-item">
+          <div
+            key={index}
+            className="d-flex flex-column align-items-center flex-shrink-0 me-3"
+            style={{ width: "240px" }}
+          >
             <Skeleton height={150} width={150} />
-            <Skeleton width={100} />
-            <Skeleton width={60} />
+            <div className="mt-2">
+              <Skeleton width={100} />
+            </div>
+            <div className="mt-1">
+              <Skeleton width={60} />
+            </div>
           </div>
         ))}
-      </div>
       </div>
     </div>
   );
