939b5410614c61913e0f0d51fb7b860fd59de346
navaskm <navaskm228@gmail.com>
2025-11-18 14:10:00 -0800

    fix bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,6 @@
+export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
+
 import { Suspense, lazy } from "react";
 import { SkeletonTheme } from "react-loading-skeleton";
 import { fetchProduct,fetchScrollingProduct } from "@/data/productdata";
